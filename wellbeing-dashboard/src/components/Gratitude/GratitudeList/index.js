f9fc06653ed3344a1452720aa3389817eebc7507
GabrielFdeOliveira <gbl.diesel@hotmail.com>
2022-11-29 15:46:28 +0000

    test

--- wellbeing-dashboard/src/components/Gratitude/GratitudeList/index.js
+++ wellbeing-dashboard/src/components/Gratitude/GratitudeList/index.js
@@ -5,13 +5,13 @@ import { FaTrashAlt } from "react-icons/fa";
 /** Component responsible for displaying the information contained on  "Gatitude list" stored at our DB. */
 export default function GratitudeList({
 
-/* Reveives the current state of our list and the "handleDelete" fn passed down by props in the Gratitude component */   
+/* Reveives the current state of our "gratitude list" and the "handleDelete" fn passed down by props in the Gratitude component */   
   list,
   handleDelete,
   // handleEdit
 }) {
 
-
+/* Here we map through our "list" prop, creating a new array and adding a new item for each one held by the current state, then using dot notation to grab the ID of each item and its text content. With that done we recycle the ID value as a key for this component at <li> also the the text content as argument for the "handleDelete"  */
   return (
     <ul className="GratitudeList">
       {list.length > 0 &&  //this line 12 conditional rendering. Checks if there are any items in the list. If there are, it will continue with the rest of this JS code. If not, it won't render anything and it also won't give an error.
