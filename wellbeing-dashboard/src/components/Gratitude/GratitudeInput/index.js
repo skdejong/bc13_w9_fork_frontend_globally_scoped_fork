e02f15d9c06f8f131cd514afde06e113e03cad09
GabrielFdeOliveira <gbl.diesel@hotmail.com>
2022-11-29 15:18:16 +0000

    Producing our JSDoc file and adding comments to the code.

--- wellbeing-dashboard/src/components/Gratitude/GratitudeInput/index.js
+++ wellbeing-dashboard/src/components/Gratitude/GratitudeInput/index.js
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
 import "./index.css";
 
+/** Component responsible for capturing the user input, using fn received as props by it's parent component */
 export default function GratitudeInput({ handleCreate }) {
+
+/* State created to receive the text provided by the user */  
   const [userInput, setUserInput] = useState("");
 
+/* Fn that captures whatever the user types and update our "userInput" state */  
   function handleChange(e) {
     setUserInput(e.target.value);
   }
 
+/* Fn that adds the "userInput" state to our list using the "handleCreate" fn and reset the text field to blank */  
   function handleClick(userInput) {
     handleCreate(userInput);
     setUserInput("");
   }
 
+/* Here we set some props to be passed ahead, also define that the "handleClick" fn will be called "onChange" and assign the "userInput" content to the "value" prop. */  
   return (
     <section className="GratitudeInput">
       <input
@@ -20,6 +26,7 @@ export default function GratitudeInput({ handleCreate }) {
         value={userInput}
         onChange={handleChange}
       />
+{/* Here we defined that "onClick" the "handleClick" fn will be called using the "userInput" state as argument. */}
       <button
         className="addButton"
         onClick={() => {
--- wellbeing-dashboard/src/components/Gratitude/GratitudeList/index.js
+++ wellbeing-dashboard/src/components/Gratitude/GratitudeList/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import "./index.css";
 import { FaTrashAlt } from "react-icons/fa";
 
+/** Component responsible for displaying the information contained on  "Gatitude list" stored at our DB. */
 export default function GratitudeList({
+
+/* Reveives the current state of our list and the "handleDelete" fn passed down by props in the Gratitude component */   
   list,
   handleDelete,
   // handleEdit
 }) {
+
+
   return (
     <ul className="GratitudeList">
       {list.length > 0 &&  //this line 12 conditional rendering. Checks if there are any items in the list. If there are, it will continue with the rest of this JS code. If not, it won't render anything and it also won't give an error.
