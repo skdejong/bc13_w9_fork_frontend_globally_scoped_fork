439fe2af3c996be2be86e3d56f12dfafcde61699
emiliawil <emiliawilliamson@pm.me>
2022-11-29 16:29:10 +0000

    Gratitudes component testing in Cypress

---
+++ wellbeing-dashboard/src/components/Gratitude/GratitudeList/index.cy.js
@@ -0,0 +1,13 @@
+// import GratitudeList from "./index";
+
+// describe("<GratitudeList/>", () => {
+//   beforeEach(() => {
+//     cy.mount(<GratitudeList />);
+//   });
+//   it("Gets list", () => {
+//     cy.request("http://localhost:3000/api/gratitudes").as("list");
+//     cy.get("@list").should((response) => {
+//       expect(response.body).to.have.property("headers");
+//     });
+//   });
+// });
--- wellbeing-dashboard/src/components/Gratitude/GratitudeList/index.js
+++ wellbeing-dashboard/src/components/Gratitude/GratitudeList/index.js
@@ -8,29 +8,36 @@ export default function GratitudeList({
   // handleEdit
 }) {
   return (
-    <ul className="GratitudeList">
-      {list.length > 0 &&  //this line 12 conditional rendering. Checks if there are any items in the list. If there are, it will continue with the rest of this JS code. If not, it won't render anything and it also won't give an error.
-        list.map((listItem) => {
-          return (
-            <li className="gratitudeListItem" key={listItem.id}>
-              {listItem.gratitude}
-              <button
-                className="gratitudeDeleteButton"
-                onClick={() => {
-                  handleDelete(listItem.id);
-                }}
-              >
-                <FaTrashAlt />
-              </button>
-            </li>
-          );
-          // handleEdit={() => {
-          //   handleEdit(index);
-          // }}
-          // handleDelete={() => {
-          //   handleDelete(index);
-          // }}
-        })}
-    </ul>
+    <div>
+      {list === undefined && (
+        <span data-cy="empty" className="empty">
+          No gratitudes here.
+        </span>
+      )}
+      <ul className="GratitudeList" data-cy="list">
+        {list.length > 0 && //this line 12 conditional rendering. Checks if there are any items in the list. If there are, it will continue with the rest of this JS code. If not, it won't render anything and it also won't give an error.
+          list.map((listItem) => {
+            return (
+              <li className="gratitudeListItem" key={listItem.id}>
+                {listItem.gratitude}
+                <button
+                  className="gratitudeDeleteButton"
+                  onClick={() => {
+                    handleDelete(listItem.id);
+                  }}
+                >
+                  <FaTrashAlt />
+                </button>
+              </li>
+            );
+            // handleEdit={() => {
+            //   handleEdit(index);
+            // }}
+            // handleDelete={() => {
+            //   handleDelete(index);
+            // }}
+          })}
+      </ul>
+    </div>
   );
 }
