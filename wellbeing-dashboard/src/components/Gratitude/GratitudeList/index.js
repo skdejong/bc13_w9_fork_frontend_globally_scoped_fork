f02d35587b74828dcc6787ef4ef7bfde817333e0
GabrielFdeOliveira <gbl.diesel@hotmail.com>
2022-11-29 16:12:21 +0000

    Comments added to all components but Activities

--- wellbeing-dashboard/src/components/Gratitude/GratitudeList/index.js
+++ wellbeing-dashboard/src/components/Gratitude/GratitudeList/index.js
@@ -11,7 +11,7 @@ export default function GratitudeList({
   // handleEdit
 }) {
 
-/* Here we map through our "list" prop, creating a new array and adding a new item for each one held by the current state, then using dot notation to grab the ID of each item and its text content. With that done we recycle the ID value as a key for this component at <li> also the the text content as argument for the "handleDelete"  */
+/* Here we map through our "list" prop, creating a new array and adding a new item for each one held by the current state, then using dot notation to grab the ID of each item and its text content. With that done we recycle the ID value as unique key for each <li> tag element and display the "listItem.gratitude" as the <li> text content itself.  */
   return (
     <ul className="GratitudeList">
       {list.length > 0 &&  //this line 12 conditional rendering. Checks if there are any items in the list. If there are, it will continue with the rest of this JS code. If not, it won't render anything and it also won't give an error.
@@ -19,12 +19,15 @@ export default function GratitudeList({
           return (
             <li className="gratitudeListItem" key={listItem.id}>
               {listItem.gratitude}
+
+              { /* Here we define that "onClick" our button element will call the "handleDelete" fn using the ID we got from our .map method as argument */ }
               <button
                 className="gratitudeDeleteButton"
                 onClick={() => {
                   handleDelete(listItem.id);
                 }}
               >
+                {/* This is hte icon we imported for the delete button */}
                 <FaTrashAlt />
               </button>
             </li>
