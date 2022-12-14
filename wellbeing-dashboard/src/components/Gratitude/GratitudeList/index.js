41e093c60a7f78aaa8ca146c50cbd2dbeff6ed42
skdejong <stephaniekdejong@gmail.com>
2022-12-14 19:32:29 +0000

    Added more comments on the code

--- wellbeing-dashboard/src/components/Gratitude/GratitudeList/index.js
+++ wellbeing-dashboard/src/components/Gratitude/GratitudeList/index.js
@@ -3,18 +3,21 @@ import "./index.css";
 import { FaTrashAlt } from "react-icons/fa";
 
 /**
- * Component responsible for displaying the information contained on  "Gatitude list" stored at our DB.
+ * Component responsible for displaying the information contained on "Gratitude list" stored in our DB.
  * @returns {Object}
  */
 export default function GratitudeList({
   /**
-   * Reveives the current state of our "gratitude list" and the "handleDelete" fn passed down by props in the Gratitude component */
+   * Reviews the current state of our "gratitude list" and the "handleDelete" function passed down by props in the Gratitude component */
   list,
   handleDelete,
   // handleEdit
 }) {
   /**
-   * Here we map through our "list" prop, creating a new array and adding a new item for each one held by the current state, then using dot notation to grab the ID of each item and its text content. With that done we recycle the ID value as unique key for each <li> tag element and display the "listItem.gratitude" as the <li> text content itself. */
+   * Here we map through our "list" prop, creating a new array and adding a new item for each one held by the current state. 
+   * Then we use dot notation to grab the ID of each item and its text content. 
+   * With that done, we recycle the ID value as a unique key for each <li> tag element and 
+   * display the "listItem.gratitude" as the <li> text content itself. */
   return (
     <div>
       {list === undefined && (
@@ -23,7 +26,7 @@ export default function GratitudeList({
         </span>
       )}
       <ul className="GratitudeList" data-cy="list">
-        {list.length > 0 && //this line is a conditional rendering. Checks if there are any items in the list. If there are, it will continue with the rest of this JS code. If not, it won't render anything and it also won't give an error.
+        {list.length > 0 && //this line is conditional rendering. It checks if there are any items in the list. If there are, it will continue with the rest of this JS code. If not, it won't render anything and it also won't give an error.
           list.map((listItem) => {
             return (
               <li className="gratitudeListItem" key={listItem.id}>
